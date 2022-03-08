102e6b243cf3a20c0f2da6a359caf4b6539c1adf
vr6we <vr6we@LAPTOP-RC84REDT>
2022-03-08 02:01:08 +0200

    Add project files.

---
+++ GenericSortedListPractice/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace GenericSortedListPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool breakFlag = false;
+            SortedIdNamesList<int, string> idNames = new SortedIdNamesList<int, string>();
+            while (!breakFlag)
+            {
+                try
+                {
+                    Console.WriteLine("\nWhat action would you like to take in Studends List?\na - Add\nd - Remove\ne - Enumerate\nb - Contains\nc - Clear\nq - Quit");
+                    string userActionListiner = Console.ReadLine().ToLower();
+                    // -->
+                    switch (userActionListiner)
+                    {
+                        case "a":
+                            Console.WriteLine("Set the number id please - number:");
+                            int keyInput = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Set the Value please:");
+                            string valueInput = Console.ReadLine();
+                            idNames.Add(keyInput, valueInput);
+                            idNames.GetList();
+                            break;
+                        //
+                        case "d":
+                            if (idNames.Count > 0)
+                            {
+                                Console.WriteLine("Which ID To Remove?");
+                                int idNumber = int.Parse(Console.ReadLine());
+                                idNames.Remove(idNumber);
+                                idNames.GetList();
+                            }
+                            else
+                                Console.WriteLine("There is no record in students list");
+                            break;
+                        //
+                        case "e":
+                            if (idNames.Count > 0)
+                                idNames.GetList();
+                            else
+                                Console.WriteLine("There is no record in students list");
+                            break;
+                        //
+                        case "b":
+                            Console.WriteLine("Please set the search input:");
+                            string studentNameSearch = Console.ReadLine();
+                            if (idNames.Contains(studentNameSearch) == true)
+                                Console.WriteLine("The Object is exist in Key Value List");
+                            else
+                                Console.WriteLine("The Object is not exist in Key Value List");
+                            break;
+                        //
+                        case "c":
+                            idNames.Clear();
+                            break;
+                        //
+                        case "q":
+                            breakFlag = true;
+                            break;
+                        //
+                        default:
+                            Console.WriteLine("Wrong Chiose, please try again!");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("The operation is failure!");
+                    idNames.GetList();
+                }
+
+            }
+        }
+    }
+}
---
+++ GenericSortedListPractice/SortedIdNamesList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericSortedListPractice
+{
+    internal class SortedIdNamesList<IdParam, NameParam>
+    {
+        public delegate void AccountOperation();
+        private SortedList<IdParam, NameParam> _sortedList = new SortedList<IdParam, NameParam>();
+        public int Count { get { return _sortedList.Count; } }
+        public void GetList()
+        {
+            foreach (var kvp in _sortedList)
+                Console.WriteLine("ID: {0}, Student Name: {1}", kvp.Key, kvp.Value);
+        }
+        //
+        public void Add(IdParam id, NameParam name)
+        {
+            _sortedList.Add(id, name);
+        }
+        //
+        public void Remove(IdParam number)
+        {
+            _sortedList.Remove(number);
+        }
+        //
+        public bool Contains(NameParam name)
+        {
+            return _sortedList.ContainsValue(name);
+        }
+        public void Clear()
+        {
+            _sortedList.Clear();
+        }
+    }
+}
